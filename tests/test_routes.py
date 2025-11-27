09c4dd3f413803c83b27185a2d1210a119b9dca2
Maha-AbdulMukhtar <mahashabdulmukhtar@gmail.com>
2025-11-27 02:42:58 -0500

    Add test case for security headers (TDD)

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -19,6 +19,9 @@ DATABASE_URI = os.getenv(
 
 BASE_URL = "/accounts"
 
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
+
+
 
 ######################################################################
 #  T E S T   C A S E S
@@ -114,7 +117,7 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_unsupported_media_type(self):
-        """It should not Create an Account when sending the wrong media type"""
+            """It should not Create an Account when sending the wrong media type"""
         account = AccountFactory()
         response = self.client.post(
             BASE_URL,
@@ -124,7 +127,7 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     def test_get_account(self):
-    """It should Read a single Account"""
+        """It should Read a single Account"""  
     account = self._create_accounts(1)[0]
     resp = self.client.get(
         f"{BASE_URL}/{account.id}", content_type="application/json"
@@ -133,21 +136,21 @@ class TestAccountService(TestCase):
     data = resp.get_json()
     self.assertEqual(data["name"], account.name)
 
-def test_get_account_not_found(self):
-    """It should not Read an Account that is not found"""
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
     resp = self.client.get(f"{BASE_URL}/0")
     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-def test_get_account_list(self):
-    """It should Get a list of Accounts"""
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
     self._create_accounts(5)
     resp = self.client.get(BASE_URL)
     self.assertEqual(resp.status_code, status.HTTP_200_OK)
     data = resp.get_json()
     self.assertEqual(len(data), 5)
 
-def test_update_account(self):
-    """It should Update an existing Account"""
+    def test_update_account(self):
+        """It should Update an existing Account"""
     account = self._create_accounts(1)[0]
     updated_data = {"name": "Updated Name", "email": "updated@test.com"}
     resp = self.client.put(
@@ -157,10 +160,22 @@ def test_update_account(self):
     )
     self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
-def test_delete_account(self):
-    """It should Delete an Account"""
+    def test_delete_account(self):
+        """It should Delete an Account"""
     account = self._create_accounts(1)[0]
     resp = self.client.delete(f"{BASE_URL}/{account.id}")
     self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
+    def test_security_headers(self):
+    """It should return security headers"""
+    response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    headers = {
+        'X-Frame-Options': 'SAMEORIGIN',
+        'X-Content-Type-Options': 'nosniff',
+        'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+        'Referrer-Policy': 'strict-origin-when-cross-origin'
+    }
+    for key, value in headers.items():
+        self.assertEqual(response.headers.get(key), value)
 
